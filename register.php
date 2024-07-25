cf142f2abe66c964c3464723e451319bef3d14ef
Tamilarasan <121883832+tamilarasanm26@users.noreply.github.com>
2024-07-25 18:26:50 +0530

    Update register.php

--- register.php
+++ register.php
@@ -4,9 +4,9 @@ include 'config.php';
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-    $role = 'user'; // Set the default role as 'user'
+    $role = 'user'; 
 
-    // Check if the username already exists
+
     $stmt = $conn->prepare("SELECT * FROM users WHERE username = :username");
     $stmt->bindParam(':username', $username);
     $stmt->execute();
